44d36131d861062c13e20977fe87468caaac6759
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-04 19:21:38 +0530

    [vidyadhar hudge]Add:Validate pin code for index number

---
+++ PinCodeProblem/PinCodePattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PinCodeProblem
+{
+    class PinCodePattern
+    {
+        public static string PINCODEPATTERN="^[1-9][0-9]{5}$";
+        public bool validatePinCode(string pinCode)
+        {
+            return Regex.IsMatch(pinCode, PINCODEPATTERN);
+        }
+    }
+}
--- PinCodeProblem/Program.cs
+++ PinCodeProblem/Program.cs
@@ -7,6 +7,10 @@ namespace PinCodeProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to pin code program");
+            Console.WriteLine("Enter the pin code =");
+            String pinCode=Console.ReadLine();
+            PinCodePattern pinCodePattern = new PinCodePattern();
+            Console.WriteLine(pinCodePattern.validatePinCode(pinCode));
         }
     }
 }
