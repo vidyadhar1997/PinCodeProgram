bfdea39a9d62b4611604e8171d7ba00108466bcf
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-04 20:31:12 +0530

    [vidyadhar hudge]Add:Validate pin code for taking space after 3 digit of pincode

--- PinCodeProblem/PinCodePattern.cs
+++ PinCodeProblem/PinCodePattern.cs
@@ -8,9 +8,14 @@ namespace PinCodeProblem
     class PinCodePattern
     {
         public static string PINCODEPATTERN="^[1-9][0-9]{5}$";
+        public static string PINCODEPATTERN1 = "^[1-9]{1}[0-9]{2}[ ][0-9]{3}$";
         public bool validatePinCode(string pinCode)
         {
             return Regex.IsMatch(pinCode, PINCODEPATTERN);
+        }
+        public bool validatePinCodeFoeSpace(string pinCode1)
+        {
+            return Regex.IsMatch(pinCode1, PINCODEPATTERN1);
         }
     }
 }
--- PinCodeProblem/Program.cs
+++ PinCodeProblem/Program.cs
@@ -9,8 +9,12 @@ namespace PinCodeProblem
             Console.WriteLine("Welcome to the pin code program");
             Console.WriteLine("Enter pin code =");
             String pinCode=Console.ReadLine();
+            Console.WriteLine("Enter pin code for validating space =");
+            String pinCode1 = Console.ReadLine();
             PinCodePattern pinCodePattern = new PinCodePattern();
             Console.WriteLine(pinCodePattern.validatePinCode(pinCode));
+            Console.WriteLine(pinCodePattern.validatePinCode(pinCode1));
+
         }
     }
 }
