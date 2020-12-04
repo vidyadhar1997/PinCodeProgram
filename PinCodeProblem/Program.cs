b41aa34e49f64da9d52f7dfac2b51e52a5614409
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-04 18:53:10 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ PinCodeProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PinCodeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to pin code program");
+        }
+    }
+}
