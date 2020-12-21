b9e92c1ecbe92e9fa5e869a02d332ae9bfdf403f
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-21 15:01:15 +0530

    [vidyadhar hudge]Refactor:Space after each methode

--- PinCodeProblem/PinCodePattern.cs
+++ PinCodeProblem/PinCodePattern.cs
@@ -9,10 +9,22 @@ namespace PinCodeProblem
     {
         public static string PINCODEPATTERN="^[1-9][0-9]{5}$";
         public static string PINCODEPATTERN1 = "^[1-9]{1}[0-9]{2}[ ][0-9]{3}$";
+
+        /// <summary>
+        /// Validates the pin code.
+        /// </summary>
+        /// <param name="pinCode">The pin code.</param>
+        /// <returns></returns>
         public bool validatePinCode(string pinCode)
         {
             return Regex.IsMatch(pinCode, PINCODEPATTERN);
         }
+
+        /// <summary>
+        /// Validates the pin code foe space.
+        /// </summary>
+        /// <param name="pinCode1">The pin code1.</param>
+        /// <returns></returns>
         public bool validatePinCodeFoeSpace(string pinCode1)
         {
             return Regex.IsMatch(pinCode1, PINCODEPATTERN1);
